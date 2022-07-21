b98c4d6fa29d9325cd2186904202685170817a84
Jabo Fecht <jifecht@gmail.com>
2022-07-21 13:08:13 +0200

    Add file to calculate solution

---
+++ helpers/getSolutionWord.js
@@ -0,0 +1,7 @@
+import words from '../files/words'
+
+export const getSolutionWord = () => {
+  const randomNumber = Math.floor(Math.random() * words.length)
+  const word = words[randomNumber]
+  return String(word).toUpperCase()
+}
