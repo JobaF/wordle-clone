9363595ea1df81d685a868b064e7ceebd1bdcba1
Jabo Fecht <jifecht@gmail.com>
2022-07-21 13:08:56 +0200

    Add functionality to set global solution word

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import GuessesView from './guessesView'
+import { solutionWordAtom } from '../helpers/atomDefinitions'
+import { useAtom } from 'jotai'
+import { useEffect } from 'react'
+import { getSolutionWord } from '../helpers/getSolutionWord'
 
 export default function Home() {
+  const [solution, setSolution] = useAtom(solutionWordAtom)
+
+  useEffect(() => {
+    setSolution(getSolutionWord)
+  }, [])
+
   return (
     <div className="mt-6">
       <GuessesView />
+      {solution}
     </div>
   )
 }
