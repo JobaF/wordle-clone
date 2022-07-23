abd87352b672ae259e46e660f6856cb507b2e312
Jabo Fecht <jifecht@gmail.com>
2022-07-23 17:02:06 +0200

    Add functionality to display colors when locking in guess

--- pages/guessesView.js
+++ pages/guessesView.js
@@ -3,7 +3,17 @@ import {
   activeGuessAtom,
   activeRowAtom,
   lockedInGuessesAtom,
+  solutionWordAtom,
 } from '../helpers/atomDefinitions'
+
+const charIsInWord = (char, word) => {
+  return word.split('').includes(char)
+}
+
+const charIsInCorrectPlace = (char, index, word) => {
+  console.log(word)
+  return word.split('')[index] === char
+}
 
 const GuessesView = () => {
   const [activeGuess, setActiveGuess] = useAtom(activeGuessAtom)
@@ -26,15 +36,31 @@ const Row = ({ rowId, word }) => {
   return (
     <div className="flex justify-center gap-1">
       {[...Array(5)].map((_, i) => (
-        <Square key={i} character={word[i]} />
+        <Square key={i} character={word[i]} charId={i} rowId={rowId} />
       ))}
     </div>
   )
 }
 
-const Square = ({ character }) => {
+const Square = ({ character, charId, rowId }) => {
+  const [activeRow, setActiveRow] = useAtom(activeRowAtom)
+  const [correctWord, setCorrectWord] = useAtom(solutionWordAtom)
+  const correctCharAndIndex = character
+    ? charIsInCorrectPlace(character, charId, correctWord)
+    : false
+  const isLoggedIn = rowId < activeRow
+  const charInWord = charIsInWord(character, correctWord)
+  const whichBackground = () => {
+    if (isLoggedIn) {
+      if (correctCharAndIndex) return 'bg-green-600'
+      else if (charInWord) return 'bg-yellow-600'
+    } else return 'bg-grey-600'
+  }
+  const backgroundColor = whichBackground()
   return (
-    <div className="border-black/40 border-2 w-24 h-24 text-5xl flex justify-center items-center">
+    <div
+      className={`${backgroundColor} border-black/40 border-2 w-24 h-24 text-5xl flex justify-center items-center`}
+    >
       {character}
     </div>
   )
