81c1d72fc25a64967d80ddd08ae11f56c01934d8
Jabo Fecht <jifecht@gmail.com>
2022-07-23 16:44:32 +0200

    Being able to render input

--- pages/guessesView.js
+++ pages/guessesView.js
@@ -1,26 +1,42 @@
+import { useAtom } from 'jotai'
+import {
+  activeGuessAtom,
+  activeRowAtom,
+  lockedInGuessesAtom,
+} from '../helpers/atomDefinitions'
+
 const GuessesView = () => {
+  const [activeGuess, setActiveGuess] = useAtom(activeGuessAtom)
+  const [activeRow, setActiveRow] = useAtom(activeRowAtom)
+  const [lockedInGuesses, setLockedInGuesses] = useAtom(lockedInGuessesAtom)
   return (
     <div className="grid gap-1">
-      {[...Array(6)].map((_, i) => (
-        <Row key={i} />
-      ))}
+      {[...Array(6)].map((_, i) => {
+        if (i === activeRow) {
+          return <Row rowId={i} key={i} word={activeGuess} />
+        } else if (i < activeRow) {
+          return <Row rowId={i} key={i} word={lockedInGuesses[i]} />
+        } else return <Row rowId={i} key={i} word={''} />
+      })}
     </div>
   )
 }
 
-const Row = () => {
+const Row = ({ rowId, word }) => {
   return (
     <div className="flex justify-center gap-1">
       {[...Array(5)].map((_, i) => (
-        <Square key={i} />
+        <Square key={i} character={word[i]} />
       ))}
     </div>
   )
 }
 
-const Square = () => {
+const Square = ({ character }) => {
   return (
-    <div className="border-black/40 border-2 w-24 h-24 text-5xl flex justify-center items-center"></div>
+    <div className="border-black/40 border-2 w-24 h-24 text-5xl flex justify-center items-center">
+      {character}
+    </div>
   )
 }
 
